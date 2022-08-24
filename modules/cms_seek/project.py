bf3a289c6ff2aac1638f441f49daf6cc867e92a2
Dirk Wagner <dw@maltego.com>
2022-08-24 19:05:22 +0200

    added save setting module cmseek

--- modules/cms_seek/project.py
+++ modules/cms_seek/project.py
@@ -4,4 +4,7 @@ from modules.cms_seek.extensions import cms_seek_registry
 from transforms import UrlToCms
 
 if __name__ == "__main__":
+    cms_seek_registry.write_transforms_config()
+    cms_seek_registry.write_settings_config()
     cms_seek_registry.write_local_mtz(command="./venv/bin/python3")
+
