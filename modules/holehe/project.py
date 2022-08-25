f1ce11db7cc5d882ac50a8b818411a536ab6eea5
Dirk Wagner <dw@maltego.com>
2022-08-25 17:14:43 +0200

    added holehe

--- modules/holehe/project.py
+++ modules/holehe/project.py
@@ -1,7 +1,10 @@
 from modules.holehe.extensions import holehe_registry
 
 # Import your transforms here
-from transforms import GreetPerson
+from transforms import EmailToAlias
+from transforms import ToDetailsHOLEHE
 
 if __name__ == "__main__":
+    holehe_registry.write_transforms_config()
+    holehe_registry.write_settings_config()
     holehe_registry.write_local_mtz(command="./venv/bin/python3")
---
+++ modules/holehe/transforms/EmailToAlias.py
@@ -0,0 +1,23 @@
+from maltego_trx.transform import DiscoverableTransform
+from maltego_trx.maltego import MaltegoTransform, MaltegoMsg
+from extensions import registry
+
+from modules.holehe.extensions import holehe_registry, holehe_set
+
+
+@holehe_registry.register_transform(display_name="Email to Alias [MST]", input_entity="maltego.EmailAddress",
+                             description='Extracts the alias from an Email Address',
+                             settings=[],
+                             output_entities=["maltego.Alias"],
+                             transform_set = holehe_set)
+class EmailToAlias(DiscoverableTransform):
+    """
+    Extracts the alias from an Email Address
+    """
+
+    @classmethod
+    def create_entities(cls, request: MaltegoMsg, response: MaltegoTransform):
+
+        email = request.Value
+        alias = str(email).split("@", 1)[0]
+        response.addEntity("maltego.Alias", alias)
