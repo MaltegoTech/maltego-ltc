96308f537151211a664401e6b66583db068a282b
mg <mg@maltego.com>
2022-09-08 18:06:40 +0200

    feat()
    Adding OpenPorts scan Transform for DNSname, IP and Netblock

---
+++ modules/nmap/transforms/DNSnameToOpenPorts.py
@@ -0,0 +1,30 @@
+import os
+
+from maltego_trx.entities import Phrase, DNS, IPAddress
+from maltego_trx.maltego import MaltegoMsg, MaltegoTransform
+from maltego_trx.transform import DiscoverableTransform
+
+from modules.nmap.extensions import nmap_registry, nmap_set
+from modules.nmap.utils.commands import COMMANDS
+from modules.nmap.utils.nmap_interaction import NmapOrchestrator
+from modules.nmap.utils.toentities import dict_to_entities
+from modules.nmap.config import TOP_PORT_SCAN_NUMBER
+
+config_file_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "config.py")
+
+# TODO check that the name of every Transforms using nmap actually states the command used
+@nmap_registry.register_transform(display_name="To Open Ports (-sV)", input_entity=DNS,
+                                  description=f'Probe open ports to determine service/version info. Command: "-sV"',
+                                  output_entities=[DNS],
+                                  transform_set=nmap_set)
+class DNSnameToOpenPorts(DiscoverableTransform):
+    # TODO
+    CMD = "{target} -sV"
+
+    @classmethod
+    def create_entities(cls, request: MaltegoMsg, response: MaltegoTransform):
+        dnsname = request.Value
+        parsing_function = COMMANDS[cls.CMD]
+        cmd = cls.CMD.format(target=dnsname)
+        cont = NmapOrchestrator.execute_command(cmd, parsing_function)
+        dict_to_entities(cont, response, dns_name=dnsname)
---
+++ modules/nmap/transforms/IPToOpenPorts.py
@@ -0,0 +1,29 @@
+import os
+
+from maltego_trx.entities import Phrase, DNS, IPAddress
+from maltego_trx.maltego import MaltegoMsg, MaltegoTransform
+from maltego_trx.transform import DiscoverableTransform
+
+from modules.nmap.extensions import nmap_registry, nmap_set
+from modules.nmap.utils.commands import COMMANDS
+from modules.nmap.utils.nmap_interaction import NmapOrchestrator
+from modules.nmap.utils.toentities import dict_to_entities
+from modules.nmap.config import TOP_PORT_SCAN_NUMBER
+
+config_file_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "config.py")
+
+
+@nmap_registry.register_transform(display_name="To Open Ports (-sV)", input_entity=IPAddress,
+                                  description=f'Probe open ports to determine service/version info. Command: "-sV"',
+                                  output_entities=[IPAddress],
+                                  transform_set=nmap_set)
+class IPToOpenPorts(DiscoverableTransform):
+    # TODO TESTED
+    CMD = "{target} -sV"
+    @classmethod
+    def create_entities(cls, request: MaltegoMsg, response: MaltegoTransform):
+        ipadd = request.Value
+        parsing_function = COMMANDS[cls.CMD]
+        cmd = cls.CMD.format(target=ipadd)
+        cont = NmapOrchestrator.execute_command(cmd, parsing_function)
+        dict_to_entities(cont, response)
