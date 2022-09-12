98d1b79bed3ad4ecb6f13e60843a1281c20a18d2
mg <mg@maltego.com>
2022-09-12 17:51:07 +0200

    feat()
    Adding the possibility to do a Ping scan

--- modules/nmap/transforms/NetblockToPing.py
+++ modules/nmap/transforms/NetblockToPing.py
@@ -17,7 +17,7 @@ config_file_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "con
                                   output_entities=[IPAddress],
                                   transform_set=nmap_set)
 class NetblockToPing(DiscoverableTransform):
-    # TODO
+    # TODO TEST
     CMD = "{target} -sP"
     @classmethod
     def create_entities(cls, request: MaltegoMsg, response: MaltegoTransform):
